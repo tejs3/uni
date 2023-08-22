4d4615f5b060b95554f570d8b8cde676457f2f90
Teja <tejs.ravi02@gmail.com>
2023-08-22 21:59:10 +0530

    adding first time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+// import Cart from './components/Cart';
+import Home from './components/Home';
 
 function App() {
+//   // State for managing the cart items
+//   const [cartItems, setCartItems] = useState([]);
+
+//   // Function to add a product to the cart
+//   const addToCart = (product) => {
+//     setCartItems([...cartItems, product]);
+//   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Home/>
+      <Products/>
+
     </div>
+
+    // <Router>
+    //   <div>
+    //     {/* <Navbar cartItems={cartItems} /> */}
+    //     <Switch>
+    //       <Route exact path="/">
+    //       <Route exact path="/" component={Home} />
+    //         <Products addToCart={addToCart} />
+    //       </Route>
+    //       {/* <Route path="/cart">
+    //         <Cart cartItems={cartItems} />
+    //       </Route> */}
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 
